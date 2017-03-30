de8e58addd5b5e0d15fb5d53ed7b042a053bbe98
jacksonsix <jacksonsixmailbox@gmail.com>
2017-03-30 09:58:16 -0400

    Update logger.js
    write to file

--- logger.js
+++ logger.js
@@ -1,4 +1,7 @@
 // many users can go to this module. one copy is good now. so it should be sigleton
+var fs = require('fs');
+
+
 
 var table = [];
 var init = false;
@@ -8,6 +11,21 @@ function db(){
   return table;  
 }
 
+function write(cmd){
+ fs.writeFile("/tmp/test", cmd, function(err) {
+    if(err) {
+        return console.log(err);
+    }
+
+    console.log("The file was saved!");
+ });
+}
+
 exports.log = function(cmd){
   db().push(cmd);
+  write(cmd);
 }
+
+
+
+ 
