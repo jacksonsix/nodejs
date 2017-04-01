d9ed593f93bfd34d93406f25fb98f522219d13ea
jacksonsix <jacksonsixmailbox@gmail.com>
2017-03-31 22:51:21 -0400

    Update testenv.js

--- testenv.js
+++ testenv.js
@@ -1,9 +1,35 @@
 const http = require('http');
+const fs = require('');
+var qs = require('querystring');
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
+  if (request.method == 'POST') {
+        var body = '';
+
+        request.on('data', function (data) {
+            body += data;
+
+            // Too much POST data, kill the connection!
+            // 1e6 === 1 * Math.pow(10, 6) === 1 * 1000000 ~~~ 1MB
+            if (body.length > 1e6)
+                request.connection.destroy();
+        });
+
+        request.on('end', function () {
+            var post = qs.parse(body);
+            // use post['blah'], etc.
+              console.log(post);
+             fs.writeFile("/tmp/test", JSON.stringify(req.headers), function(err) {
+              if(err) {
+                  return console.log(err);
+              }   
+            });
+        });
+    }
+ 
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello World\n');
