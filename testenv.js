7bf1f6c0aa5b5fbc15af257cd094c08b59779a76
jacksonsix <jacksonsixmailbox@gmail.com>
2017-03-31 22:54:54 -0400

    Update testenv.js

--- testenv.js
+++ testenv.js
@@ -6,19 +6,19 @@ const hostname = '127.0.0.1';
 const port = 9990;
 
 const server = http.createServer((req, res) => {
-  if (request.method == 'POST') {
+  if (req.method == 'POST') {
         var body = '';
 
-        request.on('data', function (data) {
+        req.on('data', function (data) {
             body += data;
 
             // Too much POST data, kill the connection!
             // 1e6 === 1 * Math.pow(10, 6) === 1 * 1000000 ~~~ 1MB
             if (body.length > 1e6)
-                request.connection.destroy();
+                req.connection.destroy();
         });
 
-        request.on('end', function () {
+        req.on('end', function () {
             var post = qs.parse(body);
             // use post['blah'], etc.
               console.log(post);
