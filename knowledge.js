533bad83c6384652ab09aa66d6576aef77e330b8
jacksonsix <jacksonsixmailbox@gmail.com>
2017-09-21 10:16:41 -0400

    Update knowledge.js

--- knowledge.js
+++ knowledge.js
@@ -15,3 +15,21 @@ emitter.emit('msg1','bigdata');
 
 //The EventEmitter appears to call all listeners using the apply method. 
 //Therefore, every listener can expect to receive arguments in the same order passed to the emit function
+
+
+//javascript does not do whole scope scan
+function(){
+var a = b +4;
+var b =8;
+return a;	
+}
+//will get NaN;
+
+function(){
+var b = 8;
+var a = b+8;
+return a;	
+}
+
+//will get 16
+
