58ca627882c879328c39b52a050b76db4dd97e09
jacksonsix <jacksonsixmailbox@gmail.com>
2017-12-16 00:33:08 -0500

    Update machine.js

--- machine.js
+++ machine.js
@@ -16,22 +16,26 @@ function Machine_base(){
   this.stack = [];
   this.ops = [];
   this.controller_text ='';
-  this.libs = [];
+  this.libs = {};
   
   this.get_reg = function get_reg(reg){	 
-	  return machine.registers[reg];
+	  return this.registers[reg];
   }
   
   this.set_reg = function set_reg(reg,value){
-	  machine.registers[reg] = value;
+	  this.registers[reg] = value;
   }
 
   this.start = function start(){
 	  console.log('Machine started!');
-	  this.pc = ops[this.pcindex];
-	  while(this.pc != null){
-	    console.log(this.pc); 	
-	  }
+	  this.pc = this.ops[this.pcindex];
+	  var i = 0;
+	  while(this.pc != null && i < 200){
+	    console.log(this.pc.type); 	
+		i++;
+		this.pc.func();
+	  } 
+
   }
   
   this.get_stack = function getstack(){ return this.stack;}
@@ -40,125 +44,94 @@ function Machine_base(){
   this.advance_pc = function advance_pc(machine){
 	                              console.log('pc advanced');
 								  this.pcindex += 1;
-								  this.pc =  ops[this.pcindex];
-                             }		
+								  this.pc =  this.ops[this.pcindex];
+                             }	
+  this.install_libs  = function(libs){
+	                                this.libs = libs;
+                               }							 
 						 
 							 
 }
 
 
-
 // Assembler 
 function asseble(controller_text,machine){
 	var commands = read_controller_text(controller_text);	
+	// install libs to machine
+	var libs = setup();	
+	machine.install_libs(libs);
 	machine.ops = update_insts(commands, machine);
 }
 
 
 function update_insts(insts,machine){
-	//var pc = machine.get_reg('pc');
-	var flag = machine.get_reg('flag');
-	var stack = machine.get_stack();
-	var ops = machine.get_operations();
-	
 	insts.forEach(function(inst){
 		inst.func = make_exe_proc(inst,machine);
 	});
 	return insts;
 }
 
-//basic operation on machine
-function make_assign(cmdobj,machine){
-	var src = cmdobj.src;	
-	var value =  cmdobj.dst;
-	if(cmdobj.functionName){
-		// have a perform
-		
-	}
-	return function(){
-		var treg = machine.get_reg(src);
-		console.log('returned function');
-	}
-}
-function make_test(inst,machine,labels){
-	var condition = test_condition(inst);
-	var operation = make_operation( condition,machine,labels);
-	return function(){
-		var flag = machine.
-		advance_pc();
-	};
-}
+
+// from instruction to procedure
 function make_exe_proc(inst,machine){
-	var operator = op(inst);
+	var operator = inst.type;
 	switch(operator){
 		case 'assign':
-		   make_assign(inst,machine);
+		   return make_assign(inst,machine);
 		break;
 		case 'test':
-		   make_test(inst,machine);
+		   return make_test(inst,machine);
 		break;
 		case 'branch':
-		   make_branch(inst,machine);
+		   return make_branch(inst,machine);
 		break;
 		case 'goto':
-		   make_goto(inst,machine);
+		   return make_goto(inst,machine);
 		break;
 		case 'save':
-		   make_save(inst,machine);
+		   return make_save(inst,machine);
 		break;
 		case 'restore':
-		   make_restore(inst,machine);
+		   return make_restore(inst,machine);
 		break;
 		case 'perform':
-		   make_perform(inst,machine);
+		   return  make_perform(inst,machine);
 		break;		
+		case 'label':
+		   return  make_label(inst,machine);
+		break;	
 		default:
 		   console.log('cannot find inst ' + inst);
 		break;
 	}
 }
 
-function test_machine(){
-	var regs = ['a','b','t'];	
-	var controller_text=[];	
-	controller_text.push('begin');
-	controller_text.push('(assign t (reg b)');
-	controller_text.push('(assign t (reg a)');
-	controller_text.push('(goto (label begin)');
-	controller_text.push(null);
-	var m =  make_machine(regs);
-	console.log('machine setup!');
-	
-	asseble(controller_text,machine);
-	machine.start();
-}
 
-
-// parser
+// parser to  instruction object
 function read_controller_text(controller_text){
-	function make_inst(text){
-		var text ={};
-		text.type = 'inst';
-		return text;
+	function isLabel(line){
+		return line.indexOf('(') ==-1;
 	}
 
 	function make_label(text){
-		var text ={};
-		text.type ='label';
-		return text;
+		var obj ={};
+		obj.type ='label';
+		obj.name = text;
+		return obj;
 	}	
 	var commands = [];
 	// assume each command on each line;
 	var line ='';
 	for(var i=0; i< controller_text.length;i++){
 		line = controller_text[i];
+		if(line == null) break;
 		// check is label or inst
-		if(line.indexOf('(')!=0){
-			var inst = make_inst(line);
-			commands.push(inst);
-		}else{
+		if(isLabel(line)){			
 			var label = make_label(line);
 			commands.push(label);
+		}else{
+			var inst =  gen(line);
+			commands.push(inst);		
 		}
 	}
 	return commands;	
@@ -167,10 +140,12 @@ function read_controller_text(controller_text){
 // generate proc
 function gen(info){
 	var text = info;
-	var seprator = ['(',')'];
+	var seprator = ['(',')',' '];
+	var stack =[];
+	
 	function getNextToken(){
 		//eat up space	
-		while(text[0] ===' ' || text[0] ==='\t'){
+		while(text.length>0 && (text[0] ===' ' || text[0] ==='\t')){
 			text = text.substring(1);
 		}
 		if(text.length == 0) return null;
@@ -189,148 +164,313 @@ function gen(info){
 			return word;
 		}
 	}
-	function applyv(stk){
-		var op =stk.pop();
-		var oprand =[];
-		while(stk.length>0){
-			oprand.push(stk.pop());
-		}
-        //one instruct -> one no-arg procedure
-        //swith by op
-        		
-	}
+
 	function parse(){
-		var stack =[];		
-		var token = getNextToken();
-		if(token == null) return;
+		var token ='';		
 		do{
-			switch(token){
-				case '(':
+				token = getNextToken();
+				// return the last element on stack ?
+				if(token == null) return;
+				
+				if(token ==='('){
+				   // push to stack
+				   // the first token is operator
+				 
 				   stack.push(token);
-				break;				
-				case ')':
-				   var proc =[];
+				   
+				} else if(token===')'){
+				   // begin to pop until operator, close
+				   // then  apply operator on operands
 				   var out = stack.pop();
-				   while(out !=='('){
-					   proc.push(out);
-					   out = stack.pop();
+				   var proc = [];
+				   while(out !== '('){		
+						proc.push(out);
+						out = stack.pop();
 				   }
 				   var result = applyv(proc);
 				   stack.push(result);
-				break;				
-				default:  //word
-				   proc.push(token);
-				break;
-		    }
-            token = getNextToken();			
-			
-		}while(token != null);
+				   
+				} else{				
+					stack.push(token);
+				}
+				
+		}while(token != null )
 
+	}
+	
+		function  applyv(proc){
+		// return an object, as the parse result
+		 // pop out operator, 	 	   
+		   var obj = {};
+		  
+		   var op = proc.pop();
+		   switch(op){
+			 case 'assign':
+				 obj.type = 'assign';
+				 obj.dest = proc.pop();
+				 obj.src = proc.pop(); 
+				 if(obj.src.type ==='operation'){
+					 obj.params =[];
+					 while(proc.length>0){
+						 obj.params.push(proc.pop());
+					 }
+				 }
+				 break;
+			 case 'reg':
+				 obj.type = 'reg';
+				 obj.name = proc.pop();
+				 break;		 
+			 case 'label':
+				 obj.type = 'label';
+				 obj.name = proc.pop();
+				 break;
+			 case 'test':
+				 obj.type ='test';
+				 obj.operation = proc.pop();
+				 obj.left = proc.pop();
+				 obj.right = proc.pop();
+				 break;
+			 case 'const':
+				 obj.type ='const';
+				 obj.value = proc.pop();
+				 break;
+			 case 'op':
+				 obj.type = 'operation';
+				 obj.functionName = proc.pop();
+				 break;
+			 case 'branch':
+				 obj.type ='branch';
+				 obj.dest = proc.pop();
+				 break;
+			 case 'save':
+				 obj.type = 'save';
+				 obj.reg = proc.pop();
+				 break;
+			 case 'restore':
+				 obj.type = 'restore';
+				 obj.reg = proc.pop();
+				 break;
+			 case 'goto':
+				 obj.type = 'goto';
+				 obj.dest = proc.pop();
+				 break;			 
+		   default:
+				break;	   
+		   }  
+		   
+		   return  obj; 	   	   
 	}
 	
 	parse();	
+	var result = stack.pop();
+	return result;
 }
 
 
-function prettyprint(info){
-	var text = info;
-	var seprator = ['{','}','[',']',':','"',','];
-	function getNextToken(){
-		//eat up space	
-		while(text[0] ===' ' || text[0] ==='\t'){
-			text = text.substring(1);
-		}
-		if(text.length == 0) return null;
-		var ch = text[0];
-		if(seprator.indexOf(ch) != -1){
-			text = text.substring(1);
-			return ch;
-		}else{
-			var word ='';
-			while(seprator.indexOf(ch) == -1){
-				word += ch;				
-				if(text.length ==0) break;
-				text = text.substring(1);
-				ch = text[0];
-			}
-			return word;
-		}
+function make_operation_exp(exp,params,machine){
+	var  opfunc = machine.libs[exp.functionName];
+	var operands = params.map(function(oprand){
+		 return make_primitive_exp(oprand,machine);
+	});
+
+	return function(){
+		var ss = operands.map(function(p){
+		                return p();
+	       });
+		return opfunc.apply(this, ss);
 	}
 	
-	function parse(){
-		var stack =[];
-		var proc =[];
-		var level = 0;
-		
-		var token = getNextToken();
-		if(token == null) return;
-		do{
-			switch(token){
-				case '{':
-				   flush(proc,level);
-				   level += 1;
-				   print(token,level);
-				break;
-				case '[':
-				   flush(proc,level);
-				   level += 1;
-				   print(token,level);
-				break;
-				case '}':
-				   flush(proc,level);				
-				   print(token,level);
-				   level -= 1;
-				break;
-				case ']':	
-                   flush(proc,level);				
-				   print(token,level);
-				   level -= 1;
-				break;
-				case ':','"':
-				  proc.push(token);
-				break;
-				case ',':
-				  flush(proc,level);
-				break;
-				default:  //word
-				   proc.push(token);
-				break;
+}
+
+function make_primitive_exp(exp, machine){
+	var operation = exp.type;
+	var result ={};
+	switch(exp.type){
+		case 'const':
+		   result = function(){
+                 return exp.value;			   
 		    }
-            token = getNextToken();			
-			
-		}while(token != null);
+			break;
+      case 'label':
+          result  = function(){
+			  return exp.name;
+		  }	  
+		  break;
+	 case 'reg':
+         result  = function(){
+			  return machine.get_reg(exp.name);
+		 }
+        break;
+    default:
+	   console.log('wrong exp ' + exp);
+       break;	
+	}
+	
+	return result;
+}
 
-	}
+//basic operation on machine
+function make_assign(cmdobj,machine){
+	var src = cmdobj.src;	
+	var dest =  cmdobj.dest;	
+	var calculate ={};
 	
-	function print(segment,level){
-		if(segment.length ==0) return;
-		var space = '';
-		for(var i=0; i< level;i++){
-			space += '  ';
+	if(src.type ==='operation'){
+		calculate = make_operation_exp(src,cmdobj.params, machine);
+	}else{
+		calculate = make_primitive_exp(src,machine);
+	}
+	return function(){		
+		var val = calculate();
+		machine.set_reg(dest,val);
+		machine.advance_pc();
+	}
+}
+function make_test(inst,machine){	
+	var operation = inst.operation;
+	var func = machine.libs[operation.functionName];
+	
+	return function(){
+		var left = inst.left;
+		var right = inst.right;
+		// valueOf  , if left is const, or reg.
+		if(left.type ==='const'){
+			left = left.value;
+		}else{
+			left = machine.get_reg(left.name);
 		}
-		console.log(space + segment);
+		
+		if(right.type ==='const'){
+			right = right.value;
+		}else{
+			right = machine.get_reg(right.name);
+		}
+	
+		if(func != null){
+			 var cond = func(left,right);
+			 if(cond){
+				 machine.flag = true;
+			 }else{
+				 machine.falg = false;
+			 }
+		}		
+		machine.advance_pc();
+	};
+}
+
+function make_goto(inst,machine){
+	// find label,
+	var dest = inst.dest;	
+		
+	return function(){
+		var index = -1;
+		var staticname = '';
+		if(dest.type==='reg'){			
+			staticname = machine.get_reg(dest.name);
+		}else if(dest.type ==='label'){
+			staticname = dest.name;
+		}
+		for(var i=0; i< machine.ops.length;i++) {
+			var cmd = machine.ops[i];
+            if(cmd.type ==='label'){
+				if(cmd.name === staticname){
+					index = i;
+					break;
+				}
+			}			
+		}
+		machine.pcindex = index;
+	    machine.pc = machine.ops[index];
 	}
+}
+
+function make_label(inst,machine){
+	return function(){
+		machine.advance_pc();
+	};
+}
+
+function make_branch(inst,machine){
+	var dst = inst.dest;
+	return function(){
+		if(machine.flag){
+		   	var index = -1;
+			for(var i=0; i< machine.ops.length;i++) {
+				var cmd = machine.ops[i];
+				if(cmd.type ==='label'){
+					if(cmd.name === dst.name){
+						index = i;
+						break;
+					}
+				}			
+			}
+			machine.pcindex = index;
+			machine.pc = machine.ops[index];
+		}else{
+			machine.advance_pc();
+		}	
+	};
+}
+
+function make_save(inst,machine){
 	
-	function flush(proc,level){
-		var line='';
-	    while(proc.length >0){
-		  var ch = proc.shift();
-		  if(ch ===':' || line[line.length-1] ===':'){
-		  	line = line + ' ' +ch;  
-		  }else{
-		  	line = line  + ch;  
-		   }
-		  
-		  
-	    }
-		print(line,level);
+	return function(){
+		var val = machine.get_reg(inst.reg);
+		machine.stack.push(val);
+		machine.advance_pc();
 	}
-	
-	parse();
-	
+}
+
+function make_restore(inst,machine){
+	var regName =  inst.reg;
+	return function(){
+		var old = machine.stack.pop();
+		machine.set_reg(regName,old);
+		machine.advance_pc();
+	}
+}
+
+function make_perform(inst,machine){
+	var perform = inst.perform;
+	var proc = make_operation_exp(perform,'',machine);
+
+	return function(){
+			if(proc != null){
+		         proc();
+	       }
+		   machine.advance_pc();
+	}
 }
 
 
+function setup(){
+	var env = {};
+	env.lessthan = function(left,right){
+		return left < right;
+	}
+	env.equal = function(left,right){
+		return left == right;
+	}
+	env.bigthan = function(left,right){
+		return left > right;
+	}
+	env.add = function(left,right){
+		return left + right;
+	}
+	env.rem = function(a,b){
+		return a % b;
+	}
+	env.mul = function(a,b){
+		return a * b;
+	}
+	env.sub = function(a,b){
+		return a - b;
+	}
+	
+	return env;
+}
+
+////////////////////////////////////////////////////////       test part     //////////////////////////////////////////////////////////////////
 
 function testproc(){
 	function add(a,b){
@@ -358,28 +498,41 @@ function testproc(){
 	var ww = func(4,5);
 }
 
-function testinsts(){
-	var cmd = [];	
-	cmd.push(1);
-	cmd.push('begin')
-	cmd.push(2);
-	cmd.push('goto begin');
-	cmd.push(null);
-	
-	
-	function advance_pc(){
-		var index = cmd.indexOf(pc);
-		pc = cmd[index+1];
-	}
-	function start(){
-		var pc = cmd[0];
-		while(pc != null){
-		  console.log(pc); // if inst execute proceudre, label do nothing and get next inst
-		  
-		  advance_pc();			
-		}
+var command_text = '(assign continue (label fact-done)) ;\
+fact-loop;\
+   (test (op equal) (reg n) (const 1));\
+   (branch (label base-case));\
+   (save continue);\
+   (save n);\
+   (assign n (op sub) (reg n) (const 1));\
+   (assign continue (label after-fact));\
+   (goto (label fact-loop));\
+after-fact;\
+   (restore n);\
+   (restore continue);\
+   (assign val (op mul) (reg n) (reg val));\
+   (goto (reg continue)) ;\
+base-case;\
+   (assign val (const 1)) ;\
+   (goto (reg continue));\
+fact-done;';
 
+
+function test_machine(){
+	var regs = ['val','n','continue'];	
+	var controller_text=[];	
+     var ss = command_text.split(';');
+    for(var i=0;i< ss.length;i++){
+		controller_text.push(ss[i]);
 	}
-	start();
+	var m =  make_machine(regs);	
+	console.log('machine setup!');
+	m.set_reg('val',1);
+	m.set_reg('n',6);
+
+	asseble(controller_text,m);
+	m.start();
+	
+	  console.log('Machine finish!');
+	  console.log('result  is ' + m.get_reg('val'));
 }
-
