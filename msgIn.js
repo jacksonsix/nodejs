658f8415e98ae041176ce8faf3c7124e29e4c0fe
jacksonsix <jacksonsixmailbox@gmail.com>
2018-02-18 20:51:04 -0500

    Update msgIn.js

--- msgIn.js
+++ msgIn.js
@@ -3,7 +3,9 @@ var http = require('http');
 
 //create a server object:
 http.createServer(function (req, res) {
-	
+  var a =10;
+	 debugger;	
+	var b = 34;
   res.write('Hello World!'); //write a response to the client
   res.end(); //end the response
 }).listen(3000); //the server object listens on port 3000
